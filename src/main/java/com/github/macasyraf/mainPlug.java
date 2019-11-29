da76dd50c5f6dea999c7027d30f16f7d494d7093
macasyraf <macasyraf@gmail.com>
2019-11-28 22:38:47 -0800

    Initial Commit

---
+++ src/main/java/com/github/macasyraf/mainPlug.java
@@ -0,0 +1,15 @@
+package com.github.macasyraf;
+
+public class mainPlug {
+
+    public static void main(String[] args) {
+
+        new mainPlug().getPlug();
+    }
+
+    private void getPlug() {
+
+
+
+    }
+}
